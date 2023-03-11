de9920ce8a7d111d51053c4a9110ddb7b0b62856
Ngangolo <konengangolo54@gmail.com>
2023-03-11 23:20:39 +0100

    landing page commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Dashboard from "./pages/Dashboard"
-//import ProjectList from "./pages/ProjectList"
+import ProjectList from "./pages/ProjectList"
 import ProjectCreate from "./pages/ProjectCreate"
 import ProjectEdit from "./pages/ProjectEdit"
 import ProjectShow from "./pages/ProjectShow"
@@ -16,11 +16,11 @@ function App() {
   return (
     <Router>
       <Routes>
-          {/* <Route exact path="/"  element={<Login/>} /> */}
+          <Route exact path="/"  element={<Login/>} />
           <Route exact path="/"  element={<LandingPage/>} />
           <Route path="/register"  element={<Register/>} />
           <Route path="/dashboard"  element={<Dashboard/>} />
-          {/* <Route exact path="/list"  element={<ProjectList/>} /> */}
+          <Route exact path="/list"  element={<ProjectList/>} />
           <Route path="/create"  element={<ProjectCreate/>} />
           <Route path="/edit/:id"  element={<ProjectEdit/>} />
           <Route path="/show/:id"  element={<ProjectShow/>} />
